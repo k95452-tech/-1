f66a03d2e14029f8bb9435734d1d913f88dcd545
k95452-tech <k95452@gmail.com>
2026-03-02 13:28:46 +0800

    Add files via upload

---
+++ data/epaper/epaper-list.js
@@ -0,0 +1,27 @@
+// 電子報資料庫
+const epaperData = [
+	{
+        id: "2026-02",
+        title: "2026年第一期",// 電子報名稱
+        cover: "img/epaper/深耕計畫電子報2026第一期-1.jpg", // 封面圖
+		publishDate: "2026年02月", // 您可以手動指定日期文字
+        pages: [　// 內頁圖
+            "img/epaper/深耕計畫電子報2026第一期-1.jpg",
+            "img/epaper/深耕計畫電子報2026第一期-2.jpg",
+            "img/epaper/深耕計畫電子報2026第一期-3.jpg",
+	    "img/epaper/深耕計畫電子報2026第一期-4.jpg"
+        ]
+    	},
+	
+	{
+        id: "2025-12",
+        title: "2025年第一期",// 電子報名稱
+        cover: "img/epaper/深耕計畫電子報第一期-1.jpg", // 封面圖
+		publishDate: "2025年12月", // 您可以手動指定日期文字
+        pages: [　// 內頁圖
+            "img/epaper/深耕計畫電子報第一期-1.jpg",
+            "img/epaper/深耕計畫電子報第一期-2.jpg",
+            "img/epaper/深耕計畫電子報第一期-3.jpg"
+        ]
+    	}
+];
